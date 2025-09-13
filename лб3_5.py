523c975e88a7ffed2e4e8bb2d8d1546738d5fd7d
ArturRSO15 <artur-maliev040706@mail.ru>
2025-09-13 10:12:12 +0300

    Update лб3_5.py

--- лб3_5.py
+++ лб3_5.py
@@ -1,17 +1,17 @@
 class Student: #создание класса student 
-    def __init__(self, name, student_id):# конструктор класса-специальный метод который вызывается для создании нового объекта 
-        self.name = name#сохранение имени в атрибуте объекта 
-        self.student_id = student_id#сохранение id в атрибуте объекта 
+    def __init__(self, name, student_id):
+        self.name = name
+        self.student_id = student_id
         self.grades = []#инициализация пустого списка 
 
     def __str__(self):#метод для строкового представления объекта 
-        return f"Student {self.name} (ID: {self.student_id})"#возвращает форматированную строку с информацией о студенте 
+        return f"Student {self.name} (ID: {self.student_id})" 
 
     def __eq__(self, other):#метод для сравнения объектов на равенство 
-        return self.student_id == other.student_id#сравнивает студентов по их id 
+        return self.student_id == other.student_id
 
     def __len__(self):#метод для получения длинны объекта 
-        return len(self.grades)#возвращает количество оценок у студента 
+        return len(self.grades)
 
 
 # Пример использования
@@ -23,3 +23,4 @@ print(s1)  # Student Иван (ID: 123)
 print(s1 == s2)  # True (сравниваем по ID)
 
 print(len(s1))  # 3 (количество оценок)
+
